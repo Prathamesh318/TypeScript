5dfd8d3960c2f2a9cdcb47f2ea5349b35c0ed196
Prathamesh Veer <119663192+Prathamesh318@users.noreply.github.com>
2024-10-09 23:27:05 +0530

    Update app.ts

--- Never type/app.ts
+++ Never type/app.ts
@@ -9,7 +9,7 @@ if (typeof userInput === 'string') {
 
 function generateError(message: string, code: number): never {
   throw { message: message, errorCode: code };
-  // while (true) {}
+
 }
 
 generateError('An error occurred!', 500);
