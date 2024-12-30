6c1d7143dba171c67c0ad10d91e3e303d88df04a
Prathamesh318 <ov01809@gmail.com>
2024-12-30 18:41:02 +0530

    Sample

--- Basic Types/app.ts
+++ Basic Types/app.ts
@@ -1,9 +1,10 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number,n3:number) {
+  return n1 + n2 + n3;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const number3 = 5;
 
-const result = add(number1, number2);
+const result = add(number1, number2,number3);
 console.log("Result"+result);
